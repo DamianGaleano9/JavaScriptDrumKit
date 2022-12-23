1eee899c3924a6e4b8215cf20b51d9c0b405007d
Damian Galeano <dj.damian.b@hotmail.com>
2022-12-23 23:46:04 +0100

    play sound

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-
+    if(!audio) return;
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
 }
 
+const keys = Array.from(document.querySelectorAll('.key');
 
 
 window.addEventListener('keydown', playSound);
