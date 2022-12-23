f441b9172c23f839f9f1b0c569fc3ed4a82d636c
Damian Galeano <dj.damian.b@hotmail.com>
2022-12-24 00:04:02 +0100

    remove transition

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+
+
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
@@ -7,7 +14,7 @@ function playSound(e) {
     audio.play();
 }
 
-const keys = Array.from(document.querySelectorAll('.key');
-
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 window.addEventListener('keydown', playSound);
